f95d5e47a29d73254cc8cb8adb3063a502a7604b
Brandon <pacewicbrandon@gmail.com>
2022-03-31 08:51:29 -0700

    Inital commit

---
+++ sound.ts
@@ -0,0 +1,42 @@
+const width = 1500;
+const height = 1500;
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = width;
+canvas.height = height;
+
+let soundAnalyser: AnalyserNode;
+
+const getAudio = async () => {
+  const stream = await navigator.mediaDevices
+    .getUserMedia ({ audio: true });
+
+  const audioCtx = new AudioContext();
+  soundAnalyser = audioCtx.createAnalyser();
+  const source = audioCtx.createMediaStreamSource(stream); // Typescript problem here
+  source.connect(soundAnalyser);
+
+  soundAnalyser.fftSize = 2 ** 10;
+
+  const timeData = new Uint8Array(soundAnalyser.frequencyBinCount);
+  const frequenceData = new Uint8Array(soundAnalyser.frequencyBinCount);
+
+  drawTimeData(timeData);
+}
+
+const drawTimeData = (timeData: Uint8Array) => {
+  console.log(timeData);  
+  soundAnalyser.getByteTimeDomainData(timeData);
+
+  ctx.lineWidth = 10;
+  ctx.strokeStyle = '#ffc600';
+  ctx.beginPath();
+
+  requestAnimationFrame(() => {
+    drawTimeData(timeData);
+  });
+};
+
+getAudio();
