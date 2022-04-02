e6bb37782a59f45e16ddd0d16fe901dc6c9f0f10
Brandon <pacewicbrandon@gmail.com>
2022-04-02 16:20:23 -0700

    working time data

--- sound.ts
+++ sound.ts
@@ -4,10 +4,23 @@ const height = 1500;
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+console.log(canvas, ctx);
+
 canvas.width = width;
 canvas.height = height;
 
 let soundAnalyser: AnalyserNode;
+let bufferLength;
+
+const HSLToRGB = (h: number, s: number, l: number) => {
+  s /= 100;
+  l /= 100;
+  const k = n => (n + h / 30) % 12;
+  const a = s * Math.min(l, 1 - l);
+  const f = n =>
+    l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+  return [255 * f(0), 255 * f(8), 255 * f(4)];
+};
 
 async function getAudio() {
   const stream = await navigator.mediaDevices
@@ -19,23 +32,67 @@ async function getAudio() {
   source.connect(soundAnalyser);
 
   soundAnalyser.fftSize = 2 ** 10;
+  bufferLength = soundAnalyser.frequencyBinCount;
 
   const timeData = new Uint8Array(soundAnalyser.frequencyBinCount);
   const frequenceData = new Uint8Array(soundAnalyser.frequencyBinCount);
 
   drawTimeData(timeData);
+  drawFrequency(frequenceData);
 }
 
 const drawTimeData = (timeData: Uint8Array) => {
   console.log(timeData);  
   soundAnalyser.getByteTimeDomainData(timeData);
 
+  ctx.clearRect(0, 0, width, height);
   ctx.lineWidth = 10;
   ctx.strokeStyle = '#ffc600';
   ctx.beginPath();
 
+  const sliceWidth = width / bufferLength;
+  let x = 0;
+
+  timeData.forEach((data, i) => {
+    const z = data / 128;
+    const y = (z * height) / 2;
+
+    if (i === 0) {
+      ctx.moveTo(x, y);
+    }
+    else {
+      ctx.lineTo(x, y);
+    }
+
+    x += sliceWidth;
+  });
+
+  ctx.stroke();
+
   requestAnimationFrame(() => {
     drawTimeData(timeData);
+  });
+};
+
+const drawFrequency = (frequencyData: Uint8Array) => {
+  soundAnalyser.getByteFrequencyData(frequencyData);
+  const barWidth = (width / bufferLength) * 2.5;
+  let x = 0;
+
+  frequencyData.forEach((amount) => {
+    const percent = amount / 255;
+    const [h, s, l] = [360 / (percent * 360) - 0.5, 0.8, 0.5];
+    const barHeight = height * percent * 0.5;
+    const [r, g, b] = HSLToRGB(h, s, l);
+
+    ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
+    ctx.fillRect(x, height - barHeight, barWidth, barHeight);
+
+    x += barWidth + 2;
+  });
+
+  requestAnimationFrame(() => {
+    drawFrequency(frequencyData);
   });
 };
 
