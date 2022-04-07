c1d0ee77c9611e13b9394abb5a890262fec2a4ef
Brandon <pacewicbrandon@gmail.com>
2022-04-07 08:39:10 -0700

    update to static types

--- sound.ts
+++ sound.ts
@@ -10,13 +10,13 @@ canvas.width = width;
 canvas.height = height;
 
 let soundAnalyser: AnalyserNode;
-let bufferLength;
+let bufferLength: number;
 
-const HSLToRGB = (h: number, s: number, l: number) => {
+const HSLToRGB = (h: number, s: number, l: number): number[] => {
   s /= 100;
   l /= 100;
-  const k = n => (n + h / 30) % 12;
   const a = s * Math.min(l, 1 - l);
+  const k = n => (n + h / 30) % 12;
   const f = n =>
     l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
   return [255 * f(0), 255 * f(8), 255 * f(4)];
@@ -47,7 +47,7 @@ const drawTimeData = (timeData: Uint8Array) => {
 
   ctx.clearRect(0, 0, width, height);
   ctx.lineWidth = 10;
-  ctx.strokeStyle = '#ffc600';
+  ctx.strokeStyle = '#ffc600'; // Yellow
   ctx.beginPath();
 
   const sliceWidth = width / bufferLength;
