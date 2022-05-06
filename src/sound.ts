0b043be45dcb1a5777fee694efb907ced9c460e2
Brandon <pacewicbrandon@gmail.com>
2022-05-06 10:43:56 -0700

    Working on button commands (#8)

--- src/sound.ts
+++ src/sound.ts
@@ -1,4 +1,4 @@
-// import { hslToRgb } from './utils.js';
+import { hslToRgb } from './utils.js';
 
 const width = 1500;
 const height = 1500;
@@ -21,7 +21,7 @@ async function getAudio() {
   const source = audioCtx.createMediaStreamSource(stream);
   source.connect(soundAnalyser);
 
-  soundAnalyser.fftSize = 2 ** 13;
+  soundAnalyser.fftSize = 2 ** 11;
   bufferLength = soundAnalyser.frequencyBinCount;
 
   const timeData = new Uint8Array(soundAnalyser.frequencyBinCount);
@@ -35,27 +35,45 @@ const limit = (min: number, max: number, value: number) => {
   return Math.max(min, Math.min(max, value));
 };
 
+let timeColor = '#00ffff';
+
+const switchTimeColor = () => {
+  // cycle rgb hex colors
+  const colors = [
+    '#ff0000',
+    '#ff7f00',
+    '#ffff00',
+    '#00ff00',
+    '#00ffff',
+    '#0000ff',
+    '#7f00ff',
+    '#ff00ff',
+  ];
+
+  timeColor = colors[Math.floor(Math.random() * colors.length)];
+};
+
 const drawTimeData = (timeData: Uint8Array) => {
   soundAnalyser.getByteTimeDomainData(timeData);
 
   ctx.clearRect(0, 0, width, height);
-  ctx.lineWidth = 4.5;
-  ctx.strokeStyle = 'rgb(0, 0, 255)';
-  ctx.shadowBlur = 8;
-  ctx.shadowColor = 'grey';
+  ctx.lineWidth = 5;
+  ctx.strokeStyle = timeColor;
+  ctx.shadowBlur = 13;
+  ctx.shadowColor = 'black';
   ctx.beginPath();
   
   const sliceWidth = width / bufferLength;
   let x = 0;
 
   timeData.forEach((data, i) => {
-    if (i % 6 === 0) {
+    if (i % 8 === 0) {
       return;
     }
 
-    const z = data / 128;
+    const z = data / 130;
     let y = (z * height) / 2;
-    y = limit(300, height - 300, y);
+    y = limit(400, height - 400, y);
 
     if (i === 0) {
       ctx.moveTo(x, y);
@@ -75,7 +93,7 @@ const drawTimeData = (timeData: Uint8Array) => {
 
 const drawFrequency = (frequencyData: Uint8Array) => {
   soundAnalyser.getByteFrequencyData(frequencyData);
-  const barWidth = (width / bufferLength) * 2.5;
+  const barWidth = (width / bufferLength) * 2.3;
   let x = 0;
 
   frequencyData.forEach((amount, i) => {
@@ -84,10 +102,21 @@ const drawFrequency = (frequencyData: Uint8Array) => {
     }
 
     const percent = amount / 255;
-    let barHeight = height * percent * 0.6;
+    let barHeight = height * percent * 0.4;
     barHeight = limit(0, height - 200, barHeight);
 
-    ctx.fillStyle = 'rgb(200, 200, 200)';
+    const [h, s, l] = [360 / (percent * 500) - 0.5, 0.8, 0.5];
+    const [r, g, b] = hslToRgb(h, s, l);
+    // TODO
+    // let avg = ((r + b) / 2) * 1.5;
+
+    // if (avg < 128) {
+    //   avg = 0;
+    // }
+
+    ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
+    // TODO
+    ctx.fillRect(x, 0, barWidth, barHeight);
     ctx.fillRect(x, height - barHeight, barWidth, barHeight);
 
     x += barWidth + 2;
@@ -98,6 +127,15 @@ const drawFrequency = (frequencyData: Uint8Array) => {
   });
 };
 
+
 (() => {
   getAudio();
 })();
+
+document.onkeydown = (event) => {
+  switch (event.key) {
+    case "'":
+      switchTimeColor();
+      break;
+  }
+};
